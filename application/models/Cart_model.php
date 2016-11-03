4fae286aba47d6a0ea15a3d3c91b59321ee03ab8
Adamantios <manos_0_7@hotmail.com>
2016-11-03 20:12:33 +0200

    Fix cart model.

--- application/models/Cart_model.php
+++ application/models/Cart_model.php
@@ -9,18 +9,46 @@ class Cart_model extends CI_Model
         $this->load->database();
     }
 
+    public function create_cart($user_id, $cart)
+    {
+        return $this->db->insert('cart', array('user_id' => $user_id, 'cart' => $cart));
+    }
+
     public function get_cart($user_id)
     {
-        return $this->db->get_where('cart', array('user_id' => $user_id))->result();
+        $query = $this->db->get_where('cart', array('user_id' => $user_id));
+
+        if ($query->num_rows() == 0)
+            return FALSE;
+        else
+            return $query->result()[0]->cart;
     }
 
     public function change_cart($user_id, $cart)
     {
-        $this->db->where('user_id', $user_id);
+        $this->db->trans_start();
 
-        if ($this->db->update('cart', array('cart' => $cart)))
-            return TRUE;
-        else
+        if ($this->db->get_where('cart', array('user_id' => $user_id))->num_rows() != 0) {
+            $this->db->where('user_id', $user_id);
+
+            if (!$this->db->update('cart', array('cart' => $cart))) {
+                $this->db->trans_rollback();
+                return FALSE;
+            }
+        } else
+            if (!$this->create_cart($user_id, $cart)) {
+                $this->db->trans_rollback();
+                return FALSE;
+            }
+
+        // Complete the transaction.
+        $this->db->trans_complete();
+
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
             return FALSE;
+        }
+
+        return TRUE;
     }
 }
